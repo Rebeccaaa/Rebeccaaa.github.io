6828501587d43a0d2da1b748a8d20ca51cc21b19
Rebeccaaa <19steatonre4@wis.edu.hk>
2015-11-13 08:54:20 +0800

    v3

---
+++ game/js/index.js
@@ -0,0 +1,76 @@
+function mygame() {
+alert("Welcome To Legend Of The Memes!")
+var userName=prompt("What is your name?");
+  confirm ("Welcome " + userName +". Are you ready to begin?");
+alert("That's a nice name!")
+alert("My Name Is Dan Howell")
+alert("*Loud Bangs*")
+alert("Whoa What Was that?")
+
+var situation = prompt("hide or look outside");
+
+switch (situation) {
+
+  case "hide":
+    alert("You Died Because Your House Fell On You.");{
+        alert("YOU DIED!");}
+    break;
+
+  case "look outside":
+    alert("Oh No It's Doge!")
+        var situation2 = prompt ("run or fight"); 
+        
+        switch (situation2) {
+                
+            case "run":
+               alert("Hits A Tree And Gets A Concussion.");
+                alert("*Two Hours Later*")
+                alert("Where Am I?")
+                alert("John Cena: You're in my lab JOHNN CENAA")
+                alert("Attacks you")
+                var situation3 = prompt ("attack or defend")
+                
+                 switch(situation3) {
+                        
+                    case "attack":
+                        alert("You Missed Your Attack And Died");
+                        alert("YOU DIED!");
+                        break;
+                        
+                    case "defend":
+                        alert("You Defened John Cena's Attack");
+                        alert("ATTACK");
+                        alert("You Killed John Cena!");
+                        alert(" YOU WON!!!");
+                        break;
+                 
+                       
+                }
+        break; 
+                
+               
+            case "fight":
+                alert("You Killed Doge!");
+                alert("You Go Back With Your Normal Life, Until...");
+                alert("Doges Minions That Are Pepe Poisoned Your Food.");
+                alert("YOU DIED") ;
+                break;
+                
+            default:
+                alert("That's not a valid answer!");
+        }
+   
+        
+}
+alert("Thank you for playing!")
+var rate = prompt ("How Much Do You Rate It out of 10");
+if (rate<5)
+{
+    alert("That's quite low!");
+}
+else
+{
+    alert("Thank You! Thanks for playing!");
+}
+alert ("Bye!")
+}
